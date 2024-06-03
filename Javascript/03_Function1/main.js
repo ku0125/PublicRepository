49a3d0eddc7d80bc8a0f90a96a7459c816d534e9
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-03 11:04:56 +0900

    Javascriptサイト作成

---
+++ Javascript/03_Function1/main.js
@@ -0,0 +1,15 @@
+// 関数
+// プログラム(処理)の塊のことです。
+
+function click1() {
+    document.getElementById('p1').textContent = '実行ボタンが押されました。'
+}
+
+function click2() {
+    document.getElementById('p2').textContent = '実行ボタンが押されました。'
+}
+
+function reset() {
+    document.getElementById('p1').textContent = '１つ目の内容'
+    document.getElementById('p2').textContent = '２つ目の内容'
+}
---
+++ Javascript/04_Variable/main.js
@@ -0,0 +1,43 @@
+// 変数
+// 変数の定義
+// var let
+// 基本はletを使う
+
+// 変数の定義と初期化
+let name = '田中太郎'
+
+// let name = '田中太郎'の処理を2行に分解
+let name2
+name2 = '田中太郎2'
+
+// 変数名の付け方
+// 意味のある（意図の伝わる）名前をつけてください。
+
+// ２単語以上の場合
+let personalName
+
+// 命名規則について
+// キャメルケース（camelCase）
+// ローワーキャメルケース（lowerCamelCase）
+// アッパーキャメルケース（UpperCamelCase）
+// スネークケース（snake_case）（SNAKE_CASE）
+// 変数は基本的にローワーキャメルケースを利用する。
+
+// 変数の代入について
+// = を利用して右辺の値を左辺に代入する
+// 左辺に値が入っていた場合は上書きされる
+
+// 変数の取得について
+// 変数名を書くだけ
+
+
+// JavaScriptでHTMLに出力する処理
+let output = document.getElementById('output')
+
+// 入力する文字を定義
+let msg = 'あいうえお'
+
+// 文字を出力する
+output.textContent = msg
+
+
