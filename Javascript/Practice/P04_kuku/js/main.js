994ca071ca280decf5b278777d3487baebb62e2d
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-06 15:32:26 +0900

    課題4修正

--- Javascript/Practice/P04_kuku/js/main.js
+++ Javascript/Practice/P04_kuku/js/main.js
@@ -28,31 +28,31 @@ for (let i = 1; i < 10; i++) {
     output2.innerHTML += '<br>'
 }
 
-// 課題3-1
-// 九九表を出力
-// tableタグを使用
+// // 課題3-1
+// // 九九表を出力
+// // tableタグを使用
 
-// output3.innerHTMLでタグをどんどん追加する
-output3.innerHTML = '<table border=1>'
-output3.innerHTML += '<tr>'
+// // output3.innerHTMLでタグをどんどん追加する
+// output3.innerHTML = '<table border=1>'
+// output3.innerHTML += '<tr>'
 
-for (let i = 1; i < 10; i++) {
+// for (let i = 1; i < 10; i++) {
 
-    for (let j = 1; j < 10; j++) {
-        result = i * j
-        output3.innerHTML += `<td>${result}</td>`
-    }
-    output3.innerHTML += '</tr>'
-}
-output3.innerHTML += '</table>'
-// 動かない（？？？？？？）
+//     for (let j = 1; j < 10; j++) {
+//         result = i * j
+//         output3.innerHTML += `<td>${result}</td>`
+//     }
+//     output3.innerHTML += '</tr>'
+// }
+// output3.innerHTML += '</table>'
+// // 動かない（？？？？？？）
 
 // 課題3-2
 // 九九表を出力
 // tableタグを使用
 
 // tableHTMLの箱を作ってタグをどんどん追加する
-tableHTML = '<table border=1>'
+let tableHTML = '<table border=1>'
 tableHTML += '<tr>'
 
 for (let i = 1; i < 10; i++) {
