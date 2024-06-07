a8f034e56da59996e1777a38388d30e6f1f9abf9
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-07 15:23:53 +0900

    EX3追加

--- Javascript/EX03_WebAPI/main.js
+++ Javascript/EX03_WebAPI/main.js
@@ -4,6 +4,7 @@
 let output = document.getElementById('output')
 
 function capture(){
+    output.alt = "画像取得中"
     // データの取得（非同期通信）
     // 現代の通信では必須
     fetch('https://dog.ceo/api/breeds/image/random')
