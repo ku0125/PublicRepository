a0124dc6b7e7710bdd151f0e7041b69ad70757ae
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-09 15:16:31 +0900

    スマートフォン対応

--- Javascript/EX03_WebAPI/main.js
+++ Javascript/EX03_WebAPI/main.js
@@ -1,18 +1,21 @@
 // WebAPI
 
 // 出力準備
-let output = document.getElementById('output')
+let output = document.getElementById('output');
+let captureButton = document.getElementById('captureButton');
 
 function capture(){
-    output.alt = "画像取得中"
+    output.alt = "画像取得中";
     // データの取得（非同期通信）
     // 現代の通信では必須
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(Response => Response.json())
         .then(json => {
-            console.log(json)
-            output.src = json.message
-        })
-
-
+            console.log(json);
+            output.src = json.message;
+        });
 }
+
+// ボタンのクリックイベントとタッチイベントの両方に対応
+captureButton.addEventListener('click', capture);
+captureButton.addEventListener('touchstart', capture);
