e1937c838b1ea1ee0422a3e8d65f571ea57b74cf
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-09 15:48:35 +0900

    ボタン連打対策追加

--- Javascript/EX03_WebAPI/main.js
+++ Javascript/EX03_WebAPI/main.js
@@ -5,6 +5,8 @@ let output = document.getElementById('output');
 let captureButton = document.getElementById('captureButton');
 
 function capture(){
+    // ボタンを無効化
+    captureButton.disabled = true;
     output.alt = "画像取得中";
     // データの取得（非同期通信）
     // 現代の通信では必須
@@ -13,6 +15,8 @@ function capture(){
         .then(json => {
             console.log(json);
             output.src = json.message;
+            // 画像の読み込み完了後にボタンを有効化
+            captureButton.disabled = false;
         });
 }
 
