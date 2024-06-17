5c1b4fa2d638d48d26484c5c788cf364be0dbe8b
ku0125 <0037.keisuke.ura@gmail.com>
2024-06-17 11:24:29 +0900

    14,15_クラス説明分割,TOP更新

---
+++ Javascript/15_Class2/main.js
@@ -0,0 +1,24 @@
+// クラスについて
+
+// 出力先の要素を習得
+let output = document.getElementById('output')
+
+// クラスの宣言
+class Fan {
+    // プロパティ(状態)
+
+    // 羽根の枚数
+    blades
+    // 風力
+    windPower
+    // 電源
+    power
+    // 首振り
+    swing
+    // 状態の出力先
+    output
+
+}
+
+// インスタンス化
+let fan01 = new Fan()
