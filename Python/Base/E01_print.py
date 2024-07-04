c0ce6bbc8ee6cb73f8b6bb1412ca862ff176821b
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-04 13:31:26 +0900

    .gitignore追加　venv除外

--- Python/Base/E01_print.py
+++ Python/Base/E01_print.py
@@ -7,3 +7,7 @@ int02 = int(input("値2を入力してください。\n>"))
 
 # 複数のデータ出力
 print(int01,int02,"abc")
+# 区切り文字の変更(defaultは" "(半角スペース))
+print(int01,int02,"abc",sep="/")
+# 語尾文字の変更(defaultは"\n"(改行コード))
+print(int01,int02,"abc",end="/")
