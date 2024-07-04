f2e1c6ff0052d134213f419128a15fa841d479dc
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-04 13:27:54 +0900

    venv削除

--- Python/Base/03_Operator.py
+++ Python/Base/03_Operator.py
@@ -42,3 +42,32 @@ print(int03)
 # インクリメント、デクリメントは存在しない
 # i = 10
 # i++
+
+print("--- 比較演算子 ---")
+bool01 = 10 != 1
+print(bool01)
+
+print(10 == 1)
+print(10 != 1)
+print(10 >= 1)
+print(10 >  1)
+print(10 <= 1)
+print(10 < 1)
+
+print("--- 論理演算子 ---")
+# かつ
+print(True and True)
+# または
+print(True or True)
+# 否定
+print(True and True)
+# 等価
+print(True and True)
+# 不等価
+print(True and True)
+
+# 配列の中に存在するか確認する演算子
+print(3 in [1,2,3,4,5,6,7,8,9])
+print(3 not in [1,2,3,4,5,6,7,8,9])
+
+print("abc" , 100)
