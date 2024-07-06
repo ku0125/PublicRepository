ca0e71d50b4026b2a481f5cbf9a32ebac487989e
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-06 19:53:54 +0900

    PyP02_電卓修正

--- Python/Base/Practice/P02_Calc.py
+++ Python/Base/Practice/P02_Calc.py
@@ -6,23 +6,26 @@ root.title("電卓")
 root.geometry("500x500")
 
 
+# {e1.get()}→文字列として取得している　数値の変数として持ってくる場合は中かっこ不要
+
+
 def add():
-    result = {e1.get()} + {e2.get()}
+    result = float(e1.get()) + float(e2.get())
     result_label.config(text=f"{result}")
 
 
 def sub():
-    result = {e1.get()} + {e2.get()}
+    result = float(e1.get()) - float(e2.get())
     result_label.config(text=f"{result}")
 
 
 def mul():
-    result = {e1.get()} + {e2.get()}
+    result = float(e1.get()) * float(e2.get())
     result_label.config(text=f"{result}")
 
 
 def div():
-    result = {e1.get()} + {e2.get()}
+    result = float(e1.get()) / float(e2.get())
     result_label.config(text=f"{result}")
 
 
@@ -30,6 +33,8 @@ def div():
 e1 = tk.Entry(bg="#FF0000")
 # 入力フォームの設置
 e1.pack(anchor=tk.NW)
+
+print(type({e1.get()}))
 
 # 入力フォームのインスタンス化
 e2 = tk.Entry(bg="#FF0000")
@@ -42,10 +47,10 @@ bt1.pack(anchor=tk.NW)
 bt2 = tk.Button(text="減算", command=sub)
 # ボタンの設置
 bt2.pack(anchor=tk.NW)
-bt3 = tk.Button(text="除算", command=mul)
+bt3 = tk.Button(text="乗算", command=mul)
 # ボタンの設置
 bt3.pack(anchor=tk.NW)
-bt4 = tk.Button(text="乗算", command=div)
+bt4 = tk.Button(text="除算", command=div)
 # ボタンの設置
 bt4.pack(anchor=tk.NW)
 
