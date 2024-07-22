8b96b2f8ff60ff2debf4c0b430ad1be709c58227
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-22 16:11:23 +0900

    Python10_12追加

--- Python/Flask/Practice/P01_EasyCalc/app.py
+++ Python/Flask/Practice/P01_EasyCalc/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 # インスタンス生成
 app = Flask(__name__)
@@ -6,27 +6,54 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("pages/index.html")
+    err = request.args.get("errid")
+    return render_template("pages/index.html", errid=err)
 
 
-@app.route("/result", methods=["get"])
+@app.route("/result")
 def result():
     num1 = request.args.get("num1")
     num2 = request.args.get("num2")
-    result = float(num1) + float(num2)
-    return render_template("pages/result.html", result=result)
+    op = request.args.get("op")
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+    except TypeError:
+        # err="入力エラー：整数を入れてください。"
+        # 結果にエラーが出る
+        # return render_template("pages/result.html",ans=ans)
+        # return render_template("pages/index.html",errmsg=err)
+        return redirect(url_for("index", errid="E001"))
+    except ValueError:
+        # err="入力エラー：整数を入れてください。"
+        # 結果にエラーが出る
+        # return render_template("pages/result.html",ans=ans)
+        # return render_template("pages/index.html",errmsg=err)
+        return redirect(url_for("index", errid="E001"))
+    else:
+        return calc(num1, num2, op)
 
 
-# # GETパラメータ(おまけ)
-# # http://localhost:5001/get?id=100
-# @app.route("/get")
-# def get():
-#     id = request.args.get("id")
-#     return f'ページ{id}<a href="/">ホームへ戻る</a>'
+def calc(num1, num2, op):
+    if op == "add":
+        ans = num1 + num2
+    elif op == "minus":
+        ans = num1 - num2
+    elif op == "multi":
+        ans = num1 * num2
+    elif op == "divide":
+        try:
+            ans = num1 / num2
+        except ZeroDivisionError:
+            return redirect(url_for("index", errid="E002"))
+    else:
+        return redirect(url_for("index", errid="E003"))
+    return render_template("pages/result.html", ans=ans)
+
 
 if __name__ == "__main__":
     # デバッグモード
     app.debug = True
     # ホストとポートの指定
     # WEBサーバー実行
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=8080)
