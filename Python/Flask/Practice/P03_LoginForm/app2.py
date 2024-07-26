33438c71e587137fa8527bf8d30154f6bd8e7cdf
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-26 19:48:52 +0900

    PyFl_P03_一応動作

--- Python/Flask/Practice/P03_LoginForm/app2.py
+++ Python/Flask/Practice/P03_LoginForm/app2.py
@@ -1,12 +1,11 @@
 # MethodBase
-from flask import Blueprint, redirect, render_template, session, url_for
+from flask import Blueprint, flash, redirect, render_template, session, url_for
 
 # Blueprintの名称：LoginSystem
 # モジュール名：LoginSystem
 # MethodBase
 from flask.views import MethodView
 from forms import InputForm
-
 
 # url_prefixがメインの階層になる
 Login = Blueprint("LoginSystem", __name__, url_prefix="/LoginSystem")
@@ -21,22 +20,21 @@ class LoginSystem(MethodView):
     # postでのアクセス時
     def post(self):
         form = InputForm()
-        # POST
         if form.validate_on_submit():
             session["id"] = form.id.data
             session["pw"] = form.pw.data
-            return redirect(url_for("LoginSystem.login"))
-        # GET
-        if "name" in session:
-            form.id.data = session["id"]
-        if "email" in session:
-            form.pw.data = session["pw"]
-
-        if session.get("id") == "test" and session.get("pw") == "test":
-            return render_template("pages/success.html")
-
-        # GETリクエストの場合、またはフォームの値がバリデーションを通過しなかった場合
+            if session.get("id") == "test" and session.get("pw") == "test":
+                return redirect(url_for("LoginSystem.success"))
+            else:
+                flash("ログインに失敗しました。", "error")
+                return redirect(url_for("LoginSystem.login"))
         return render_template("pages/login.html", form=form)
 
 
 Login.add_url_rule("/", view_func=LoginSystem.as_view("login"))
+
+
+# 成功画面のルート
+@Login.route("/success")
+def success():
+    return render_template("pages/success.html")
