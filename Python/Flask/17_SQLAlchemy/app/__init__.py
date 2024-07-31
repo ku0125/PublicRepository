06ef07285190605c6e336cbebdaf1140c572158e
ku0125 <0037.keisuke.ura@gmail.com>
2024-07-31 11:57:10 +0900

    PyFl_17SQLAlchemy更新

--- Python/Flask/17_SQLAlchemy/app/__init__.py
+++ Python/Flask/17_SQLAlchemy/app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 # インスタンス生成
 app = Flask(__name__)
+# Sessionのシークレットキー
+app.secret_key = b"hit"
 # SQLAlchemyの接続先サーバーの設定
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ToDo.db"
 # Flaskインスタンスと連動設定
--- Python/Flask/17_SQLAlchemy/app/main.py
+++ Python/Flask/17_SQLAlchemy/app/main.py
@@ -1,8 +1,9 @@
-from flask import redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for
 
 from app import ToDo, app, db
 
 
+# データ一覧の表示ページ
 @app.route("/")
 def index():
     dbses = db.session
@@ -12,6 +13,7 @@ def index():
     return render_template("index.html", datas=res)
 
 
+# データの挿入処理
 @app.route("/create", methods=["POST"])
 def create():
     text = request.form.get("text")
@@ -19,4 +21,47 @@ def create():
     dbses = db.session
     dbses.add(data)
     dbses.commit()
+    flash("データの作成に成功しました")
+    # 全件表示のページへリダイレクト
     return redirect(url_for("index"))
+
+
+# データの更新処理
+@app.route("/update", methods=["POST"])
+def update():
+    text = request.form.get("text")
+    id = request.form.get("id")
+    dbses = db.session
+    # １件検索
+    res = dbses.query(ToDo).get(id)
+    if res is None:
+        # idが存在しない時
+        flash("指定されたIDが存在しないので失敗しました。")
+        return redirect(url_for("index"))
+    else:
+        # データを更新する処理
+        res.text = text
+        dbses.commit()
+        flash("データの更新に成功しました")
+        # 全件表示のページへリダイレクト
+        return redirect(url_for("index"))
+
+
+# データの削除処理
+@app.route("/delete")
+def delete():
+    id = request.args.get("id")
+    dbses = db.session
+    # １件検索
+    res = dbses.query(ToDo).get(id)
+    if res is None:
+        # idが存在しない時
+        flash("指定されたIDが存在しないので失敗しました。")
+        return redirect(url_for("index"))
+    else:
+        # データを削除する処理
+        dbses.delete(res)
+        dbses.commit()
+        flash("データの削除に成功しました。")
+        # 全件表示のページへリダイレクト
+        return redirect(url_for("index"))
