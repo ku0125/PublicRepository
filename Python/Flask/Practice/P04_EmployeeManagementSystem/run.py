4183c2f986613ebae349da56630304074279e3d3
ku0125 <0037.keisuke.ura@gmail.com>
2024-09-01 23:15:37 +0900

    waitress導入

--- Python/Flask/Practice/P04_EmployeeManagementSystem/run.py
+++ Python/Flask/Practice/P04_EmployeeManagementSystem/run.py
@@ -1,5 +1,7 @@
 from app import app
+from waitress import serve
 
 if __name__ == "__main__":
     # WEBサーバー実行
-    app.run(host="0.0.0.0", port=8080)
+    # app.run(host="0.0.0.0", port=8080)
+    serve(app, host='0.0.0.0', port=80)
